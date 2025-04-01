351f6ee31a388a885407658af95522a3895c51dd
你的GitHub用户名 <GitHub>
2025-04-01 14:30:15 +0800

    feat: Decrypted Text

--- components/above/index.jsx
+++ components/above/index.jsx
@@ -1,5 +1,5 @@
 import O from "@/components/icons/O";
-
+import DecryptedText from './DecryptedText'
 const Above = () => {
   return (
     <div className="relative h-screen overflow-hidden">
@@ -7,13 +7,35 @@ const Above = () => {
       <div className="absolute left-2 sm:left-4 top-12 sm:top-24">
         <div className="flex flex-col items-start">
           <div className="above">
-            f
+          <DecryptedText 
+            text="f"
+            animateOn="view"
+            revealDirection="center"
+            speed={500}
+          />
             <O className="w-[80px] h-[48px] sm:w-[140px] sm:h-[80px] md:w-[180px] md:h-[100px] lg:w-[212px] lg:h-[120px]" />
-            cus
+            <DecryptedText 
+              text="cus"
+              animateOn="view"
+              revealDirection="center"
+              speed={500}
+            />
           </div>
           <div className="pl-1 text-xs leading-tight sm:pl-2 sm:text-sm md:text-base lg:text-lg">
-            I am currently engaged in AI & WEARABLES
-            <div>collaboration is welcome</div>
+            <DecryptedText 
+              text="I am currently engaged in AI & WEARABLES" 
+              animateOn="view"
+              revealDirection="center"
+              speed={500}
+            />
+            <div>
+            <DecryptedText 
+              text="collaboration is welcome"
+              animateOn="view"
+              revealDirection="center"
+              speed={500}
+         
+            /></div>
           </div>
         </div>
       </div>
@@ -21,22 +43,59 @@ const Above = () => {
       {/* explore */}
       <div className="absolute right-2 sm:right-4 top-64 sm:top-40 md:top-48 lg:top-60">
         <div className="above">
-          expl
+          <DecryptedText 
+            text="expl"
+            animateOn="view"
+            revealDirection="center"
+            speed={500}
+          />
           <O className="w-[80px] h-[48px] sm:w-[140px] sm:h-[80px] md:w-[180px] md:h-[100px] lg:w-[212px] lg:h-[120px]" />
-          re
+          <DecryptedText 
+            text="re"
+            animateOn="view"
+            revealDirection="center"
+            speed={500}
+          />
         </div>
       </div>
 
       {/* break through */}
       <div className="absolute -translate-x-1/2 left-1/2 bottom-12 sm:bottom-16 md:bottom-20 lg:bottom-24">
         <div className="above whitespace-nowrap">
-          break thr
+          <DecryptedText 
+          text="break "
+          animateOn="view"
+          revealDirection="center"
+          speed={500}/>
+          <DecryptedText 
+            text="thr"
+            animateOn="view"
+            revealDirection="center"
+            speed={500}/>
+
           <O className="w-[80px] h-[48px] sm:w-[140px] sm:h-[80px] md:w-[180px] md:h-[100px] lg:w-[212px] lg:h-[120px]" />
-          ugh
+          <DecryptedText 
+            text="ugh"
+            animateOn="view"
+            revealDirection="center"
+            speed={500}
+          />
         </div>
         <div className="pr-1 text-xs leading-tight sm:pr-2 sm:text-sm md:text-base lg:text-lg text-end">
-          This is a portfolio website specialize in
-          <div>observing and exploring human behavior</div>
+          <DecryptedText 
+            text="This is a portfolio website specialize in"
+            animateOn="view"
+            revealDirection="center"
+            speed={500}
+          />
+          <div>
+              <DecryptedText 
+                text="observing and exploring human behavior"
+                animateOn="view"
+                revealDirection="center"
+                speed={500} 
+            />        
+              </div>
         </div>
       </div>
     </div>
